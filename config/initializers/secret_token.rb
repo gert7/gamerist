0e3c5b89831927a7fea13eb70442a3c96a6b1392
Gert Oja <gertoja@hotmail.com>
2013-09-13 21:56:04 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gamerist::Application.config.secret_key_base = '3d911ad4c7fdb0a650550ed6abd0c749c7c838c20f949ebccd051c34219d5fc5ad8385d3ef140b251532ea141ae56e3a0d2fad310774b2330d99066820e3543e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gamerist::Application.config.session_store :cookie_store, key: '_gamerist_session'
