8012db7ac37c7076d9206724d41e7a1b550235a8
Gert Oja <gertoja@hotmail.com>
2013-09-17 11:29:46 +0300

    Removed users

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130909150327) do
-
-  create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
