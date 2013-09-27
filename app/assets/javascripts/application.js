14c6d7764e902ce7838a132d2833f95819f6665d
Gert Oja <gertoja@hotmail.com>
2013-09-27 22:14:38 +0300

    Changing header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree ./sitewide
+//= require ./welcome/jquery.animate-shadow-min
+//= require ./welcome/welcome
