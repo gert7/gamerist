8e065895509832b94981f438ca264c89898a97d7
Gert Oja <gertoja@hotmail.com>
2013-10-28 13:59:21 +0200

    Adding print to test

--- www/test/integration/user_signup_test.rb
+++ www/test/integration/user_signup_test.rb
@@ -1,15 +1,18 @@
 require 'test_helper'
 
 class UserSignupTest < ActionDispatch::IntegrationTest
-  fixtures :users
-
   test "sign up and log out" do
+	print "Starting https"
     https!
+	print "getting users sign up page"
     get "/users/sign_up"
+	print "Testing success"
     assert_response :success
-    
+    print "posting sign up"
     post_via_redirect "/users/sign_up", email: "useroni@mail.ch", password: "alpacat2", password_confirmation: "alpacat2"
+	print "checking user logged in"
     assert user_logged_in?
-    assert_redirected_to "welcome#index"
+	print "checking redirect"
+    assert_redirected_to "welcome/index"
   end
 end
