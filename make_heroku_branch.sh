7aec6faf4804bda5bdadb8f22a5ad5a822bf97e4
Gert Oja <gertoja@hotmail.com>
2013-10-29 15:46:55 +0200

    Finished heroku script

--- make_heroku_branch.sh
+++ make_heroku_branch.sh
@@ -1,21 +1,23 @@
 set -e
 
+cd gamerist
+
 git checkout master
 
 rm -rf /tmp/heroku_branch_script
 mkdir /tmp/heroku_branch_script
-cp -r ./* /tmp/heroku_branch_script/
+cp -R ./* /tmp/heroku_branch_script/
 
 git checkout heroku
 rm -rf *
-cp -r /tmp/heroku_branch_script/www ./
+cp -R /tmp/heroku_branch_script/www/* ./
 
 git add -A
 read -p "Please add a commit message: " herokucmes
 git commit -m"$herokucmes"
 git push origin heroku
-git push heroku heroku:master
+# git push heroku heroku:master
 
 rm -rf /tmp/heroku_branch_script
 git checkout master
-read -p "Checkout complete. You should now see the master branch."
+echo "Checkout complete. You should now see the master branch."
