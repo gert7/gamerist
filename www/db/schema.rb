72b1a25df9e3772fbf798129fbbcba00eb8bffd5
Gert <gert@gert-VirtualBox.(none)>
2014-05-07 15:34:58 +0300

    Trying

--- www/db/schema.rb
+++ www/db/schema.rb
@@ -13,4 +13,37 @@
 
 ActiveRecord::Schema.define(version: 20131121143439) do
 
+# Could not dump table "accounts" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0x23c61b>
+
+# Could not dump table "delayed_jobs" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0x6f36da>
+
+# Could not dump table "games" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0x1099a3e>
+
+# Could not dump table "maps" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0x1963b79>
+
+# Could not dump table "paypals" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0xf01077>
+
+# Could not dump table "rooms" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0x16bc9a7>
+
+# Could not dump table "rulesets" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0xd3d621>
+
+# Could not dump table "servers" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0xb32bb6>
+
+# Could not dump table "steamids" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0x1c85f78>
+
+# Could not dump table "transactions" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0x103eee0>
+
+# Could not dump table "users" because of following NoMethodError
+#   undefined method `default_function' for #<ActiveRecord::ConnectionAdapters::PostgreSQLColumn:0x1e8310d>
+
 end
