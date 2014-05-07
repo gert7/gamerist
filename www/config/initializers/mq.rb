2ef39e1c7992dc9cf1efa09a9d381dee5eddb48b
Gert <gert@gert-VirtualBox.(none)>
2014-05-07 14:35:04 +0300

    Investigating test failures -- RabbitMQ

--- www/config/initializers/mq.rb
+++ www/config/initializers/mq.rb
@@ -1,8 +1,8 @@
-conn = MarchHare.connect
-ch = conn.create_channel
-q  = ch.queue("", :auto_delete => true)
-x  = ch.topic("boopsi", :auto_delete => true)
-q.bind(x, routing_key: "amq.rabbitmq.trace")
+#conn = MarchHare.connect
+#ch = conn.create_channel
+#q  = ch.queue("", :auto_delete => true)
+#x  = ch.topic("boopsi", :auto_delete => true)
+#q.bind(x, routing_key: "amq.rabbitmq.trace")
 
 #  q.subscribe do |metadata, payload|
 #    puts "Received #{payload}"
