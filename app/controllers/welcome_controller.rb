151724f9e6eea26439ad392a7837fb11ef484de7
Gert <gertoja1@gmail.com>
2015-04-14 23:42:04 +0300

    sql welcome

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    Admin.destroy_all
-    Admin.create! do |a|
-      a.email     = "admin@admin.com"
-      a.password  = "administrator"
-      a.password_confirmation = "administrator"
+    Admin.find(email: "admin@admin.com").destroy
+    if(Rails.env.development?)
+      Admin.create! do |a|
+        a.email     = "admin@admin.com"
+        a.password  = "administrator"
+        a.password_confirmation = "administrator"
+      end
     end
   end
 end
