2ea463c800b8df6c72c7c8afd9ecf6dafc79645f
Gert <gertoja1@gmail.com>
2015-04-14 21:11:03 +0300

    got assets working for production??

---
+++ www/public/assets/tooltips-f88f15ab2c4234db3942b8f72972c44c.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){function t(t){$(t).mouseover(function(){{var t=$(this).find(".helper_tooltip").width();$(this).width()}$(".helper_tooltip .tooltip_triangle").css("margin-left",""+(t/2-10)+"px")})}t(".tooltip_wrapper")});
