f18f6f3ff6ed87ab47726f10e172a4e8147c29f2
Gert <gertoja1@gmail.com>
2015-04-15 18:53:47 +0300

    remove public assets also

--- make_heroku_branch.sh
+++ make_heroku_branch.sh
@@ -7,8 +7,6 @@ cp -r www/** ../gameristh/.
 
 cd ../gameristh
 
-RAILS_ENV=production bundle exec rake assets:precompile
-
 git add -A
 read -p "Please add a commit message: " herokucmes
 git commit -m"$herokucmes"
