1a460a739fa75f538e8b2d36cd46b42990a8942c
gert7 <gertoja@hotmail.com>
2015-11-29 14:28:38 +0200

    Added start_development.sh

---
+++ start_development.sh
@@ -0,0 +1,32 @@
+REDIS_VERSION=redis-3.0.5
+
+sudo apt-get install curl -y
+
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+
+\curl -sSL https://get.rvm.io | bash -s stable
+
+source $HOME/.rvm/scripts/rvm
+
+rvm install jruby-9.0.4.0
+
+rvm use jruby-9.0.4.0 --default
+
+gem install bundler
+
+./nodeinstall.sh
+
+\curl -O http://download.redis.io/releases/$REDIS_VERSION.tar.gz
+
+mv redis-3.0.5.tar.gz $HOME/Desktop/$REDIS_VERSION.tar.gz
+
+cd $HOME/Desktop
+
+tar -xvf $REDIS_VERSION.tar.gz
+
+rm $REDIS_VERSION.tar.gz
+
+cd $REDIS_VERSION
+
+make
+
