23f0517818c914821c6d44cd9ec8f36699e0569a
gert7 <gertoja1@gmail.com>
2016-05-05 22:05:01 +0300

    Moved development to sqlite

--- www/db/schema.rb
+++ www/db/schema.rb
@@ -13,99 +13,96 @@
 
 ActiveRecord::Schema.define(version: 20160331123205) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "accounts", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "countrycode",   limit: 255
-    t.string   "nickname",      limit: 255
+    t.string   "countrycode"
+    t.string   "nickname"
     t.date     "dob"
-    t.string   "firstname",     limit: 255
-    t.string   "lastname",      limit: 255
-    t.string   "paypaladdress", limit: 255
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "paypaladdress"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace",     limit: 255
+    t.string   "namespace"
     t.text     "body"
-    t.string   "resource_id",   limit: 255, null: false
-    t.string   "resource_type", limit: 255, null: false
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
     t.integer  "author_id"
-    t.string   "author_type",   limit: 255
+    t.string   "author_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
-  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",               default: 0, null: false
-    t.integer  "attempts",               default: 0, null: false
-    t.text     "handler",                            null: false
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
     t.text     "last_error"
     t.datetime "run_at"
     t.datetime "locked_at"
     t.datetime "failed_at"
-    t.string   "locked_by",  limit: 255
-    t.string   "queue",      limit: 255
+    t.string   "locked_by"
+    t.string   "queue"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "modifiers", force: :cascade do |t|
-    t.string   "key",        limit: 255
-    t.string   "value",      limit: 255
+    t.string   "key"
+    t.string   "value"
     t.boolean  "active"
     t.boolean  "recent"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "payouts", force: :cascade do |t|
-    t.string   "batchid",    limit: 255
+    t.string   "batchid"
     t.integer  "points"
-    t.decimal  "subtotal",               precision: 16, scale: 2
-    t.decimal  "total",                  precision: 16, scale: 2
-    t.decimal  "margin",                 precision: 16, scale: 2
-    t.string   "currency",   limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.decimal  "subtotal",   precision: 16, scale: 2
+    t.decimal  "total",      precision: 16, scale: 2
+    t.decimal  "margin",     precision: 16, scale: 2
+    t.string   "currency"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.integer  "user_id"
   end
 
   create_table "paypals", force: :cascade do |t|
-    t.decimal  "amount",                 precision: 8, scale: 2
-    t.decimal  "subtotal",               precision: 8, scale: 2
-    t.decimal  "tax",                    precision: 8, scale: 2
+    t.decimal  "amount",     precision: 8, scale: 2
+    t.decimal  "subtotal",   precision: 8, scale: 2
+    t.decimal  "tax",        precision: 8, scale: 2
     t.integer  "state"
     t.integer  "user_id"
-    t.string   "sid",        limit: 255
-    t.string   "redirect",   limit: 255
+    t.string   "sid"
+    t.string   "redirect"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "country"
@@ -135,7 +132,7 @@ ActiveRecord::Schema.define(version: 20160331123205) do
   end
 
   create_table "steamids", force: :cascade do |t|
-    t.string   "steamid",    limit: 255
+    t.string   "steamid"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -154,35 +151,35 @@ ActiveRecord::Schema.define(version: 20160331123205) do
     t.datetime "updated_at"
   end
 
-  add_index "transactions", ["user_id"], name: "index_transactions_on_user_id", using: :btree
+  add_index "transactions", ["user_id"], name: "index_transactions_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
+    t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "relevantgames"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "usertraces", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "timestamp"
-    t.string   "ipaddress", limit: 255
+    t.string   "ipaddress"
   end
 
 end
